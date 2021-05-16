71d0feb5f18ff5f0c79f8af37c8d1430932d1151
githubアカウントのユーザー <m.naotomachida@gmail.com>
2021-05-16 21:03:55 +0900

    apiをいじりました

--- src/components/atoms/Cards/MovieCard.tsx
+++ src/components/atoms/Cards/MovieCard.tsx
@@ -7,18 +7,18 @@ const MovieCard = (props) => {
 
   return (
     <>
-      <div className="shadow-2xl text-center transform transition duration-300 ease-in-out hover:scale-110 bg-gray-100 border-4 border-gray-400">
+      <div className="shadow-2xl bg-gray-700 text-center transform transition duration-300 ease-in-out hover:scale-110 bg-gray-100 border-4 border-gray-300 ">
         <img
           src={image}
           alt=""
           className="relative bg-cover min-w-full h-80 "
         />
-        <div className="absolute inset-x-0 CardImg px-2 pt-24 ">
-          <h2 className="flex justify-center items-center text-2xl text-gray-100 font-bold pt-2 h-12">
+        <div className="absolute inset-x-0 CardImg px-2 pt-24 opacity-100 sm:opacity-0 hover:opacity-100">
+          <h2 className="flex justify-center items-center text-2xl text-gray-100 font-bold pt-2 h-12 ">
             {title}
           </h2>
         </div>
-        <div className="flex justify-center pb-6">
+        <div className="flex justify-center pb-6 bg-opacity">
           <Drapdown />
         </div>
       </div>
